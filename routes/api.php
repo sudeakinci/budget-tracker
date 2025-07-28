18e4b749290b23396e0a551ff05d9740f59d1448
Sude Akıncı <akincisude07@gmail.com>
2025-07-28 15:49:59 +0300

    Add Laravel Sanctum for API authentication and create personal access tokens migration

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BankController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::post('logout', [AuthController::class, 'logout']);
+Route::apiResource('banks', BankController::class);
 Route::apiResource('transactions', TransactionController::class);
 Route::apiResource('users', UserController::class);
-Route::apiResource('banks', BankController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    // Route::apiResource('banks', BankController::class);
+    // Route::apiResource('transactions', TransactionController::class);
+    // Route::apiResource('users', UserController::class); // Eğer kullanıcı yönetimi gerekiyorsa
+});
