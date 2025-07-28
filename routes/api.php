afce633fb21fce68ac8925824f687b1434cffeb7
Sude Akıncı <akincisude07@gmail.com>
2025-07-28 13:41:53 +0300

    Add API routes for transactions, users, and banks; remove from web routes

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\BankController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::apiResource('transactions', TransactionController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('banks', BankController::class);
