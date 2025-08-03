ef3fac8ca2248c8d6d23fc798000c530e37fc045
Sude Akıncı <akincisude07@gmail.com>
2025-08-03 23:41:01 +0300

    refactor: reorganize route definitions for better structure and clarity

--- routes/api.php
+++ routes/api.php
@@ -4,47 +4,18 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PaymentTermController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SmsIntegrationController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('payment-terms', PaymentTermController::class);
-Route::apiResource('transactions', TransactionController::class)->except(['update']);
-Route::apiResource('users', UserController::class)->except(['store']);
-
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::post('logout', [AuthController::class, 'logout']);
-//     Route::apiResource('banks', BankController::class);
-//     Route::apiResource('transactions', TransactionController::class)->except(['update', 'destroy']);
-//     Route::apiResource('user', UserController::class)->except(['store']);
-// });
-
-Route::get('endpoints', function () {
-    $documentation = config('documentation');
-    $authEndpoints = ['register', 'login', 'logout'];
-    $resourceEndpoints = ['banks', 'accounts', 'transactions', 'transactionTypes', 'users'];
-    $routes = [];
-
-    foreach ($documentation as $key => $value) {
-        if (in_array($key, $authEndpoints)) {
-            $routes[] = [
-                'uri' => 'api/' . $key,
-                'method' => 'POST', // Assuming auth routes are POST
-                'description' => $value['description'],
-                'request' => $value['request'] ?? null,
-                'responses' => $value['responses'] ?? [],
-            ];
-        } elseif (in_array($key, $resourceEndpoints)) {
-            $routes[] = [
-                'uri' => 'api/' . $key,
-                'endpoints' => $value,
-            ];
-        }
-    }
-
-    return response()->json($routes);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('payment-terms', PaymentTermController::class);
+    Route::apiResource('transactions', TransactionController::class)->except(['update']);
+    Route::apiResource('users', UserController::class)->except(['store']);
 });
+
+Route::post('sms-integration/{id}', [SmsIntegrationController::class, 'receiveSms']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/doc', function () {
@@ -8,4 +9,21 @@ Route::get('/doc', function () {
 
 Route::get("/", function () {
     return view('welcome');
+});
+
+
+//Login
+Route::post('/login', function (AuthController $authController) {
+    return $authController->login(request());
+});
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+//Register
+Route::post('/register', function (AuthController $authController) {
+    return $authController->register(request());
+});
+Route::get('/register', function () {
+    return view('auth.register');
 });
