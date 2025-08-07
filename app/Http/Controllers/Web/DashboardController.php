a943db22731644161e406eaf352b24b9e80f1023
Sude Akıncı <akincisude07@gmail.com>
2025-08-07 09:51:29 +0300

    feat: implement transaction retrieval with adjusted amounts for user dashboard

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use \App\Models\Transaction;
+use \App\Models\User;
+use \App\Models\PaymentTerm;
+
 class DashboardController extends Controller
 {
     public function __construct()
@@ -13,6 +18,33 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('dashboard');
+        $user = Auth::user();
+
+        // get the latest 5 transactions for the user
+        $transactions = Transaction::with(['owner', 'user'])
+            ->where(function ($query) use ($user) {
+                $query->where('owner', $user->id)
+                    ->orWhere('user_id', $user->id);
+            })
+            ->select(
+                'transactions.*',
+                DB::raw(
+                    'CASE 
+                        WHEN transactions.owner = ' . $user->id . ' THEN transactions.amount * -1
+                        ELSE transactions.amount
+                        END as amount'
+                )
+            )
+            ->orderByDesc('created_at')
+            ->take(5)
+            ->get();
+
+        $users = User::where('id', '!=', $user->id)->get();
+        $paymentTerms = PaymentTerm::whereNull('created_by')
+            ->orWhere('created_by', $user->id)
+            ->get();
+
+
+        return view('dashboard', compact('users', 'transactions', 'paymentTerms'));
     }
 }
